a18a08e225950af55f9132b71ce8d54c687085da
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2019-02-10 14:33:43 -0200

    impl++

---
+++ src/main/java/com/albuquerque/daniel/pontoligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,14 @@
+package com.albuquerque.daniel.pontoligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
+	}
+
+}
+
